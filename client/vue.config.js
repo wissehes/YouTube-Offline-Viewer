c69ad81c47d04993fa21521ee7cc437c1ed35d26
Wisse <wissehes@gmail.com>
2020-08-10 14:35:45 +0200

    linting

--- client/vue.config.js
+++ client/vue.config.js
@@ -1,25 +1,29 @@
 module.exports = {
   transpileDependencies: ["vuetify"],
   devServer: {
-    host: 'localhost',
+    host: "localhost",
     hot: true,
     port: 8080,
-    proxy: { //https://cli.vuejs.org/guide/html-and-static-assets.html#disable-index-generation
-      '/*': { //everything from root
-        target: 'http://localhost:8000',
-        secure: false,
-        ws: false,
-      },
-      '/ws/': { //web sockets
-        target: 'http://localhost:8000',
-        secure: false,
-        ws: true
-      },
-      '!/': { //except root, which is served by webpack's devserver, to faciliate instant updates
-        target: 'http://localhost:8000/',
+    proxy: {
+      //https://cli.vuejs.org/guide/html-and-static-assets.html#disable-index-generation
+      "/*": {
+        //everything from root
+        target: "http://localhost:8000",
         secure: false,
         ws: false
       },
+      "/ws/": {
+        //web sockets
+        target: "http://localhost:8000",
+        secure: false,
+        ws: true
+      },
+      "!/": {
+        //except root, which is served by webpack's devserver, to faciliate instant updates
+        target: "http://localhost:8000/",
+        secure: false,
+        ws: false
+      }
     }
   }
-}
+};
