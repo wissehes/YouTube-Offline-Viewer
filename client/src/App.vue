315132a8545ddee4af4b3537f7b9c14c45896a8b
Wisse <wissehes@gmail.com>
2020-08-12 17:15:14 +0200

    added dark mode :)

--- client/src/App.vue
+++ client/src/App.vue
@@ -2,10 +2,16 @@
   <v-app>
     <v-app-bar app color="primary" dark>
       <h1>YouTube Downloader</h1>
+      <v-spacer />
+      <v-btn icon color="white" @click="toggleDarkMode">
+        <v-icon>{{ $vuetify.theme.dark ? 'mdi-brightness-7' : 'mdi-brightness-5' }}</v-icon>
+      </v-btn>
     </v-app-bar>
 
     <v-main>
-      <router-view />
+      <v-fade-transition mode="out-in">
+        <router-view />
+      </v-fade-transition>
     </v-main>
   </v-app>
 </template>
@@ -14,8 +20,19 @@
 export default {
   name: "App",
 
-  data: () => ({
-    //
-  })
+  data: () => ({}),
+  methods: {
+    toggleDarkMode() {
+      this.$vuetify.theme.dark = !this.$vuetify.theme.dark;
+      localStorage.setItem("dark", this.$vuetify.theme.dark);
+    }
+  },
+  mounted() {
+    if (localStorage.getItem("dark") == null) {
+      localStorage.setItem("dark", false);
+    } else {
+      this.$vuetify.theme.dark = localStorage.getItem("dark");
+    }
+  }
 };
 </script>
