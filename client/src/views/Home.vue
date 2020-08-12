0f8c13cf9e495421ddebc82c4476ef4a8e3b6cc5
Wisse <wissehes@gmail.com>
2020-08-12 16:40:59 +0200

    create better looking thingy with creator avatar and name

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -47,19 +47,31 @@
             max-width="400"
             :loading="!video.downloaded"
             :disabled="!video.downloaded"
-            :progress="50"
           >
             <template v-slot:progress>
               <v-progress-linear
                 :value="video.download_progress"
+                v-if="video.download_progress < 1"
               ></v-progress-linear>
+              <v-progress-linear v-else />
             </template>
+            <v-list-item>
+              <v-list-item-avatar>
+                <v-avatar>
+                  <v-img :src="video.author.avatar"></v-img>
+                </v-avatar>
+              </v-list-item-avatar>
+              <v-list-item-content>
+                <v-list-item-title class="headline">{{ video.title }}</v-list-item-title>
+                <v-list-item-subtitle>{{video.author.name}}</v-list-item-subtitle>
+              </v-list-item-content>
+            </v-list-item>
             <v-img
               class="white--text align-end"
               height="200px"
               :src="video.thumbnails[video.thumbnails.length - 1].url"
             >
-              <v-card-title>{{ video.title }}</v-card-title>
+              <v-card-title></v-card-title>
             </v-img>
 
             <!-- <v-card-subtitle class="pb-0">size</v-card-subtitle> -->
@@ -67,14 +79,7 @@
             <v-card-actions>
               <v-btn color="orange" @click="playVideo(video)" text>Play</v-btn>
               <v-spacer></v-spacer>
-              <v-btn
-                class="mx-2"
-                fab
-                dark
-                small
-                color="error"
-                @click="deleteVideo(video.id)"
-              >
+              <v-btn class="mx-2" fab dark small color="error" @click="deleteVideo(video.id)">
                 <v-icon dark>mdi-close</v-icon>
               </v-btn>
             </v-card-actions>
@@ -85,11 +90,7 @@
     <v-dialog v-model="videoDialog" width="800px">
       <VideoPlayer v-if="showPlayer" :video="selectedVideo" height="100%" />
     </v-dialog>
-    <v-snackbar
-      v-model="snackbar"
-      :color="snackbarError ? 'red' : 'primary'"
-      :timeout="5000"
-    >
+    <v-snackbar v-model="snackbar" :color="snackbarError ? 'red' : 'primary'" :timeout="5000">
       {{ snackbarMessage }}
       <template v-slot:action="{ attrs }">
         <v-btn text v-bind="attrs" @click="snackbar = false">Close</v-btn>
