6e6dc907d2fcc96faa0897feb57653feff71ac56
Wisse <wissehes@gmail.com>
2020-08-26 13:03:06 +0200

    added vue-debounce

--- client/src/main.js
+++ client/src/main.js
@@ -3,6 +3,9 @@ import App from "./App.vue";
 import "./registerServiceWorker";
 import router from "./router";
 import vuetify from "./plugins/vuetify";
+import vueDebounce from 'vue-debounce'
+
+Vue.use(vueDebounce)
 
 Vue.config.productionTip = false;
 
